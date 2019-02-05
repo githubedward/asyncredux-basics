7b5366a5c1f3bbfb015a32f3e0d2c8cc988c8e1b
estandarte.edward@gmail.com <estandarte.edward@gmail.com>
2019-02-05 15:10:15 -0500

    new commit

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import * as types from './types';
+import fetch from 'cross-fetch';
 
 // Action Creators
 // actions governed by UI interactions
@@ -26,9 +27,68 @@ export function requestsPosts(subreddit) {
 
 export function receivePosts(subreddit, json) {
   return {
-    type: type.REQUEST_POSTS,
+    type: types.REQUEST_POSTS,
     subreddit,
     posts: json.data.children.map(child => child.data),
     receivedAt: Date.now()
+  }
+}
+
+// ASYNC ACTION CREATOR
+
+// thunk action creator
+function fetchPosts(subreddit) {
+  // Thunk middleware passes the dispatch method as an argument to the function
+  // thus making it able to dispatch action itself
+  return function(dispatch) {
+    // First dispatch: the app state is update to inform that the API call is starting.
+    
+    dispatch(requestsPosts(subreddit))
+
+    // function called by the thunk middleware can return a value,
+    // that is passed on as the return value of the dispatch method
+
+    // In this case, we return a promise to wait for.
+    // This is not required by thunk middleware, but it is convenient for us.
+
+    return fetch(`https://www.reddit.com/r/${subreddit}.json`)
+      .then(response => response.json(),
+      // Do not use catch, because that will also catch
+      // any errors in the dispatch and resulting render,
+      // causing a loop of 'Unexpected batch number' errors.
+      // https://github.com/facebook/react/issues/6895
+      error => console.log('ERROR: ', error)
+      )
+      .then(json => dispatch(receivePosts(subreddit, json))
+      // We can dispatch many times!
+      // Here, we update the app state with the results of the API call.
+      )
+  }
+}
+
+function shouldFetchPosts(state, subreddit) {
+  const posts = state.postsBySubreddit[subreddit];
+  if (!posts) {
+    return true
+  } else if (posts.isFetching) {
+    return false
+  } else {
+    return posts.didInvalidate
+  }
+}
+
+export function fetchPostsIfNeeded(subreddit) {
+  // Note that the function also receives getState()
+  // which lets you choose what to dispatch next.
+
+  // This is useful for avoiding a network request if
+  // a cached value is already available.
+
+  return (dispatch, getState) => {
+    if (shouldFetchPosts(getState(), subreddit)) {
+      return dispatch(fetchPosts(subreddit))
+    } else {
+      return Promise.resolve()
+    }
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { selectSubreddit, fetchPosts, fetchPostsIfNeeded } from './actions/index';
+import rootReducer from './reducers/reducers';
+
+const loggerMiddleware = createLogger();
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(
+    thunkMiddleware, /* let us dispatch() functions */
+    loggerMiddleware /* neat middleware that logs actions */
+  )
+)
+
+store.dispatch(selectSubreddit('reactjs'))
+store
+  .dispatch(fetchPostsIfNeeded('reactjs'))
+  .then(() => console.log(store.getState()))
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
